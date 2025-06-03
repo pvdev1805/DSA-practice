7a943e10562f302337467e760cb1c86d034c7725
Phu Vo <u1153426@umail.usq.edu.au>
2025-06-03 18:06:37 +1000

    :wrench: refactor: Refactor day 2 - problem 2 - Find Invertal In Array

--- src/day2/FindIntervalInArray.java
+++ src/day2/FindIntervalInArray.java
@@ -7,46 +7,53 @@ public class FindIntervalInArray {
 	// Solution: Sliding Window
 	// Time Complexity: O(N)
 	// - N = a.length
-	// Space Complexity: O(m + k)
-	// - m = freq.size()
-	// - k = r - l + 1
-	public static int s1(int[] a, int l, int r) {
+	// Space Complexity: O(N + M) --> O(N)
+	// - M = freq.size()
+	public static int solution(int[] a, int l, int r) {
 		int n = a.length;
-		HashMap<Integer, Integer> range = new HashMap<>();
-		for(int i = l; i <= r; i++) {
-			range.put(i, 1);
-		}
-		
-		int minLength = Integer.MAX_VALUE;
+		int minLength = 1000000001;
 		int inRangeQuantity = 0;
-		int requiredCount = r - l + 1;
+		int range = r - l + 1;
 		
 		HashMap<Integer, Integer> freq = new HashMap<>();
 		
 		int leftPtr = 0;
 		for(int rightPtr = 0; rightPtr < n; rightPtr++) {
 			int rightNum = a[rightPtr];
-			freq.put(rightNum, freq.getOrDefault(rightNum, 0) + 1);
 			
-			if(range.containsKey(rightNum) && freq.get(rightNum).equals(range.get(rightNum))) {
+			if(freq.containsKey(rightNum)) {
+				freq.put(rightNum, freq.get(rightNum) + 1);
+			} else {
+				freq.put(rightNum, 1);
+			}
+			
+			if((rightNum >= l && rightNum <= r) && (freq.get(rightNum) == 1)) {
 				inRangeQuantity++;
 			}
 			
-			while(inRangeQuantity == requiredCount && leftPtr <= rightPtr) {
+			while(inRangeQuantity == range && leftPtr <= rightPtr) {
 				minLength = Math.min(minLength, rightPtr - leftPtr + 1);
 				
 				int leftNum = a[leftPtr];
 				freq.put(leftNum, freq.get(leftNum) - 1);
-				if(range.containsKey(leftNum) && freq.get(leftNum) < range.get(leftNum)) {
+				
+				if(freq.get(leftNum) == 0) {
+					freq.remove(leftNum);
+				}
+				
+				if(leftNum >= l && leftNum <= r) {
 					inRangeQuantity--;
 				}
+				
 				leftPtr++;
 			}
 		}
 		
-		return minLength == Integer.MAX_VALUE ? -1 : minLength;
+		return minLength == 1000000001 ? -1 : minLength;
 	}
 	// End - Solution
+	
+	
 	
 	public static void main(String[] args) {
 		int[] a1 = {2, 1, 4, 3, 2, 1, 1, 4};
@@ -58,8 +65,8 @@ public class FindIntervalInArray {
 		int[] a3 = {1, 3, 5, 7};
 		int l3 = 3, r3 = 5;
 		
-		System.out.println(s1(a1, l1, r1));
-		System.out.println(s1(a2, l2, r2));
-		System.out.println(s1(a3, l3, r3));
+		System.out.println(solution(a1, l1, r1));
+		System.out.println(solution(a2, l2, r2));
+		System.out.println(solution(a3, l3, r3));
 	}
 }
