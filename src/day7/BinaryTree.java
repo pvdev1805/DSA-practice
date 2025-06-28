ea55c5277f2cd222d045cdf279cdd83c0cdea01a
Phu Vo <u1153426@umail.usq.edu.au>
2025-06-28 12:38:38 +1000

    :wrench: feat: Implement findImbalancedNodes - BinaryTree - day7

--- src/day7/BinaryTree.java
+++ src/day7/BinaryTree.java
@@ -1,16 +1,99 @@
 package day7;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class BinaryTree {
 	Node root;
+	
+	private Node dfs(Node node, int value) {
+		if(node == null) return null;
+		
+		Node leftChild = dfs(node.left, value);
+		if(leftChild != null) return leftChild;
+		
+		Node rightChild = dfs(node.right, value);
+		if(rightChild != null) return rightChild;
+		
+		if(node.value == value) return node;
+		
+		return null;
+	}
+	
+	private Node bfs(Node node, int value) {
+		if(node == null) return null;
+		
+		LinkedList<Node> q = new LinkedList<>();
+		q.addLast(node);
+		
+		while(!q.isEmpty()) {
+			Node currentNode = q.getFirst();
+			q.removeFirst();
+			
+			if(currentNode.value == value) return currentNode;
+			if(currentNode.left == null && currentNode.right == null) continue;
+			if(currentNode.left != null) q.addLast(currentNode.left);
+			if(currentNode.right != null) q.addLast(currentNode.right);
+		}
+		return null;
+	}
+	
+	public Node findNode(int value) {
+//		 return dfs(root, value);
+		return bfs(root, value);
+	}
+	
+	public void insertNode(int parentNodeValue, char position, int targetValue) {
+		if(root == null) {
+			root = new Node(targetValue);
+			return;
+		}
+		
+		Node parentNode = findNode(parentNodeValue);
+		Node newNode = new Node(targetValue);
+		if(position == 'L') {
+			parentNode.left = newNode;
+		} else if(position == 'R') {
+			parentNode.right = newNode;
+		}
+	}
+	
+	public void postOrderTraversal(Node node) {
+		if(node == null) return;
+		postOrderTraversal(node.left);
+		postOrderTraversal(node.right);
+		System.out.print(node.value + " ");
+	}
 	
 	public int heightOfNode(Node node) {
 		if(node == null) return -1;
 		return Math.max(heightOfNode(node.left) + 1, heightOfNode(node.right) + 1);
 	}
 	
-	public boolean isBalanced(Node node) {
+	public int heightOfNodeBFS(Node node) {
+		if(node == null) return -1;
+		
+		LinkedList<Node> q = new LinkedList<>();
+		q.addLast(node);
+		int height = -1;
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			height++;
+			
+			for(int i = 0; i < size; i++) {
+				Node currentNode = q.getFirst();
+				q.removeFirst();
+				
+				if(currentNode.left != null) q.addLast(currentNode.left);
+				if(currentNode.right != null) q.addLast(currentNode.right);
+			}
+		}
+		
+		return height;
+	}
+	
+	public boolean isBalancedFromNode(Node node) {
 		if(node == null) return true;
 		
 		int leftHeight = heightOfNode(node.left);
@@ -18,14 +101,57 @@ public class BinaryTree {
 		
 		if(Math.abs(leftHeight - rightHeight) > 1) return false;
 		
-		return isBalanced(node.left) && isBalanced(node.right);
+		return isBalancedFromNode(node.left) && isBalancedFromNode(node.right);
 	}
 	
 	public boolean isBalance() {
-		return isBalanced(root);
+		return isBalancedFromNode(root);
 	}
 	
-	public ArrayList<Node> findImbalancedNode(){
-		return null;
+	// Slow
+	public boolean checkBalancedNode(Node node, ArrayList<Node> nodes) {
+		if(node == null) return true;
+		
+		int leftHeight = heightOfNode(node.left);
+		int rightHeight = heightOfNode(node.right);
+		
+		boolean leftBalanced = checkBalancedNode(node.left, nodes);
+		boolean rightBalanced = checkBalancedNode(node.right, nodes);
+		
+		int heightDiff = Math.abs(leftHeight - rightHeight);
+		if(heightDiff > 1) {
+			nodes.add(node);
+		}
+		
+		return leftBalanced && rightBalanced && heightDiff <= 1;
+	}
+	
+	// Slow
+	public ArrayList<Node> findImbalancedNodes(){
+		ArrayList<Node> res = new ArrayList<>();
+		checkBalancedNode(root, res);
+		return res;
+	}
+	
+	// Optimize
+	public int checkAndCollect(Node node, ArrayList<Node> nodes) {
+		if(node == null) return 0;
+		
+		int leftHeight = checkAndCollect(node.left, nodes);
+		int rightHeight = checkAndCollect(node.right, nodes);
+		
+		int heightDiff = Math.abs(leftHeight - rightHeight);
+		if(heightDiff > 1) {
+			nodes.add(node);
+		}
+		
+		return heightDiff + 1;
+	}
+	
+	// Optimize
+	public ArrayList<Node> findImbalancedNodes2(){
+		ArrayList<Node> res = new ArrayList<>();
+		checkAndCollect(root, res);
+		return res;
 	}
 }
--- src/day7/Main.java
+++ src/day7/Main.java
@@ -1,7 +1,44 @@
 package day7;
 
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
 		
+		BinaryTree tree = new BinaryTree();
+		tree.insertNode(0,'L', 10);
+		tree.insertNode(10, 'L', 5);
+		tree.insertNode(5, 'R', 7);
+		tree.insertNode(10, 'R', 15);
+		tree.insertNode(5, 'L', 2);
+		
+		tree.postOrderTraversal(tree.root);
+		System.out.println();
+		System.out.println("Is tree imbalanced: " + tree.isBalance());
+		
+		
+		
+		BinaryTree tree1 = new BinaryTree();
+		tree1.insertNode(0, 'L', 10);
+		tree1.insertNode(10, 'R', 15);
+		tree1.insertNode(15, 'R', 20);
+		tree1.insertNode(20, 'R', 25);
+		tree1.insertNode(10, 'L', 5);
+		tree1.insertNode(5, 'L', 2);
+		tree1.insertNode(2, 'L', 1);
+		
+		tree1.postOrderTraversal(tree1.root);
+		System.out.println();
+		System.out.println("Is tree imbalanced: " + tree1.isBalance());
+		
+		ArrayList<Node> imbalancedNodes = tree1.findImbalancedNodes();
+		for(Node node: imbalancedNodes) {
+			System.out.print(node.value + " ");
+		}
+		System.out.println();
+		ArrayList<Node> imbalancedNodes2 = tree1.findImbalancedNodes2();
+		for(Node node: imbalancedNodes2) {
+			System.out.print(node.value + " ");
+		}
 	}
 }
